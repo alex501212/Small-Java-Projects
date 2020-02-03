84d9187dece864d5936c31fa99c03e7390ab29a7
alex501212 <56118464+alex501212@users.noreply.github.com>
2020-02-03 17:35:20 +0000

    First commit

---
+++ LetterExercise/src/LetterExercise.java
@@ -0,0 +1,9 @@
+public class LetterExercise {
+
+    public static void main(String[] args) {
+        System.out.println("/////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"
+                + "\n==        Student Points        =="
+        + "");
+    }
+   
+}
